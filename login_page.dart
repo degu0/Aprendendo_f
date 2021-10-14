fa634a5d09f97e6e3df9a507caab5417a4306645
degu_cat <87346972+degu0@users.noreply.github.com>
2021-10-14 16:21:42 -0300

    Update login_page.dart

--- login_page.dart
+++ login_page.dart
@@ -23,49 +23,56 @@ class _LoginPageState extends State<LoginPage> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                child: Image.asset('assets/image/logo.png'),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              child: Image.asset('assets/image/logo.png'),
+            ),
+            TextField(
+              onChanged: (text) {
+                email = text;
+              },
+              keyboardType: TypeInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
               ),
-              TextField(
-                onChanged: (text) {
-                  email = text;
-                },
-                keyboardType: TypeInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              onChanged: (text) {
+                password = text;
+              },
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 10),
-              TextField(
-                onChanged: (text) {
-                  password = text;
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 15),
-              RaisedButton(
-                color: Colors.orange,
-                onPressed: () {
-                  if (email == "knoveseg@gmailcom" && password == '123456') {
-                    Navigator.of(context).pushReplacementNamed('/home');
-                  } else {
-                    print('login inválido');
-                  }
-                },
-                child: Text('Entrar'),
-              )
-            ],
-          ),
+            ),
+            SizedBox(height: 15),
+            RaisedButton(
+              color: Colors.orange,
+              onPressed: () {
+                if (email == "knoveseg@gmailcom" && password == '123456') {
+                  Navigator.of(context).pushReplacementNamed('/home');
+                } else {
+                  print('login inválido');
+                }
+              },
+              child: Text('Entrar'),
+            ),
+            RaisedButton(
+              color: Colors.orange,
+              onPressed: () {
+                Navigator.of(context).pushReplacementNamed('/cadastro');
+              },
+              child: Text('Entrar'),
+            )
+          ],
         ),
       ),
     );
